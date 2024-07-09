48d0c23dc1ae3c923fbb2e5163d706a9032da169
pcbimon <patipat.che@mahidol.edu>
2024-07-09 16:34:10 +0700

    feat: update template and field from document

--- ckanext/powerbi_embeded_resource/plugin.py
+++ ckanext/powerbi_embeded_resource/plugin.py
@@ -2,7 +2,7 @@ import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 
 
-class PowerbiEmbededResourcePlugin(plugins.SingletonPlugin):
+class PowerbiEmbededResourcePlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer, inherit=True)
     plugins.implements(plugins.IDatasetForm)
 
@@ -16,18 +16,24 @@ class PowerbiEmbededResourcePlugin(plugins.SingletonPlugin):
     def create_package_schema(self):
         schema = super(PowerbiEmbededResourcePlugin, self).create_package_schema()
         schema.update({
-            'report_id': [toolkit.get_validator('ignore_missing'),
+            'power_bi_report_id': [toolkit.get_validator('ignore_missing'),
                           toolkit.get_converter('convert_to_extras')],
         })
         return schema
 
     def update_package_schema(self, schema):
         schema.update({
-            'report_id': [toolkit.get_validator('ignore_missing'),
+            'power_bi_report_id': [toolkit.get_validator('ignore_missing'),
                           toolkit.get_converter('convert_from_extras')],
         })
         return schema
-
+    def show_package_schema(self):
+        schema = super(PowerbiEmbededResourcePlugin, self).show_package_schema()
+        schema.update({
+            'power_bi_report_id': [toolkit.get_converter('convert_from_extras'),
+                            toolkit.get_validator('ignore_missing')]
+        })
+        return schema
     def is_fallback(self):
         return True
 
